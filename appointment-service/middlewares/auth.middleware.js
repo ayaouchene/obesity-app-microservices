9af6f5b887478c5f3b426ad7d49b0d311ffb6ec9
hibalek <hiba.lekikot@univ-constantine2.dz>
2025-05-26 00:33:30 +0100

    Update auth.middleware.js

--- appointment-service/middlewares/auth.middleware.js
+++ appointment-service/middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-
+//hiba-ahlem
   module.exports = (req, res, next) => {
     const token = req.header('Authorization')?.replace('Bearer ', '');
     if (!token) {
